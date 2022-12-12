a6b933a375269ff3f8c51b7148ecce7c567fe2c2
mihneabuz <mihneabuzatu88@gmail.com>
2022-12-12 11:45:46 +0200

    added daily challenge

---
+++ leetcode/climbing_stairs/src/main.rs
@@ -0,0 +1,19 @@
+struct Solution {}
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let n = n as usize;
+        let mut dp = vec![0; n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for i in 2..n + 1 {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        dp[n]
+    }
+}
+
+fn main() {}
